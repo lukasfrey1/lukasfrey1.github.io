7ac98a26884667834ae69529512c1996711546f1
lukasfrey1 <lukasfrey03@icloud.com>
2020-05-19 14:38:41 +0200

    a

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-function timer() {
-  var countDownDate = new Date("May 29, 2020 12:00:00").getTime();
-  
-  var x = setInterval(function() {
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
-  
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  
-    document.getElementById("timer").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
-  
-  }, 1000);
-  }
+
 
      
     
